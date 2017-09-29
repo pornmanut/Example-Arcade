3e997cccabc6522c2410adeff9107aee28a7550f
pornmanut <pornmanuty@gmail.com>
2017-09-30 01:37:13 +0700

    add:draw_background

--- Draw/Function.py
+++ Draw/Function.py
@@ -4,12 +4,18 @@ SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
 
 
+def draw_background():
+    arcade.draw_lrtb_rectangle_filled(0,SCREEN_WIDTH,SCREEN_HEIGHT/2,0,arcade.color.FOREST_GREEN)
+    arcade.draw_lrtb_rectangle_filled(0,SCREEN_WIDTH,SCREEN_HEIGHT,SCREEN_HEIGHT/2,arcade.color.SKY_BLUE)
+
 def main():
     arcade.open_window(SCREEN_WIDTH,SCREEN_HEIGHT,'Function')
     arcade.set_background_color(arcade.color.WHITE)
     arcade.start_render()
+    draw_background()
 
 
+    arcade.finish_render()
     arcade.run()
 
 if __name__ == "__main__":
